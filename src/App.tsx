885252a84c9dd0ccb0f134e8747fc8a6e5ee44eb
vishal911m <vishal911m@gmail.com>
2025-03-11 15:41:13 +0530

    State

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CardList from "./Components/CardList/CardList";
+import Search from "./Components/Card/Search/Search";
 
 const App = () => {
   return (
   <div>
+    <Search />
     <CardList />
 </div>
 );
