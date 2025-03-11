a111bd10bafa03aa4c0422a8283500e5b73cb0dd
vishal911m <vishal911m@gmail.com>
2025-03-11 12:36:58 +0530

    Function Components

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { JSX } from 'react';
 import "./Card.css";
 
 interface Props {
@@ -7,7 +7,7 @@ interface Props {
   price: number
 };
 
-const Card = ({companyName, ticker, price}: Props) => {
+const Card: React.FC<Props> = ({companyName, ticker, price}: Props): JSX.Element => {
   return (
     <div className='card'>
       <img
--- src/Components/CardList/CardList.tsx
+++ src/Components/CardList/CardList.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { JSX } from 'react'
 import Card from '../Card/Card'
 
 type Props = {}
 
-const CardList = (props: Props) => {
+const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
       <Card companyName={'Apple'} ticker={'APPL'} price={110} />
