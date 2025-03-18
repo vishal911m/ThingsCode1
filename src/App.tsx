406fc2e09c0c617f10fa5a2b41a08aa994d1b7d9
vishal911m <vishal911m@gmail.com>
2025-03-18 11:42:49 +0530

    Data Flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { ChangeEvent, SyntheticEvent, useState } from "react";
 import ReactDOM from "react-dom/client";
 import CardList from "./Components/CardList/CardList";
-import Search from "./Components/Card/Search/Search";
 import { searchCompanies } from "./api";
+import Search from "./Components/Search/Search";
 
 const App = () => {
   console.log(searchCompanies("tsla"));
+
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
+    setSearch(e.target.value)
+    console.log(e);
+  }
+
+  const onClick = (e: SyntheticEvent)=>{
+    console.log(e);
+  }
+
   return (
   <div>
-    <Search />
+    <Search onClick={onClick} search={search} handleChange={handleChange} />
     <CardList />
 </div>
 );
