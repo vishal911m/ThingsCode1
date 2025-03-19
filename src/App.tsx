ba4397182757ab46c9422e2117b381d155ed3de3
vishal911m <vishal911m@gmail.com>
2025-03-19 11:55:14 +0530

    Type Narrowing

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,30 @@ import ReactDOM from "react-dom/client";
 import CardList from "./Components/CardList/CardList";
 import { searchCompanies } from "./api";
 import Search from "./Components/Search/Search";
+import { CompanySearch } from "./company";
 
 const App = () => {
-  console.log(searchCompanies("tsla"));
+  // console.log(searchCompanies("aapl"));
 
   const [search, setSearch] = useState<string>("");
+  const [searchResult, setSearchResult] = useState<CompanySearch[]>();
+  const [serverError, setServerError] = useState<string | null>(null);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
     setSearch(e.target.value)
     console.log(e);
   }
 
-  const onClick = (e: SyntheticEvent)=>{
-    console.log(e);
+  const onClick = async (e: SyntheticEvent)=>{
+    // console.log(e);
+    const result = await searchCompanies(search);
+
+    if (typeof result === "string") {
+      setServerError(result);
+    } else if (Array.isArray(result.data)) {
+      setSearchResult(result.data);
+    }
+    console.log("Search Result: ", searchResult);
   }
 
   return (
