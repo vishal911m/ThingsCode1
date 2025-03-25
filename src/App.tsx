cd1b62ddbafc418db109f4d90eca280372b3f1b6
vishal911m <vishal911m@gmail.com>
2025-03-25 17:05:01 +0530

    React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,13 @@
-import React, { ChangeEvent, SyntheticEvent, useState } from "react";
-import ReactDOM from "react-dom/client";
-import CardList from "./Components/CardList/CardList";
-import { searchCompanies } from "./api";
-import Search from "./Components/Search/Search";
-import { CompanySearch } from "./company";
-import ListPortfolio from "./Components/Portfolio/ListPortfolio/ListPortfolio";
-import AddPortfolio from "./Components/Portfolio/AddPortfolio/AddPortfolio";
+import { Outlet } from "react-router";
 import Navbar from "./Components/Navbar/Navbar";
-import Hero from "./Components/Hero/Hero";
-import "../src/index.css";
 
-const App = () => {
-  // console.log(searchCompanies("aapl"));
-
-  const [search, setSearch] = useState<string>("");
-  const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
-  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
-  const [serverError, setServerError] = useState<string | null>(null);
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>)=>{
-    setSearch(e.target.value)
-    console.log(e);
-  };
-
-  const onPortfolioCreate = (e: any)=>{
-    e.preventDefault();
-    console.log(e);
-    const exists = portfolioValues.find((value)=> value === e.target[0].value);
-    if (exists) return;
-    const updatedPortfolio = [...portfolioValues, e.target[0].value];
-    setPortfolioValues(updatedPortfolio);
-  }
-
-  const onPortfolioDelete = (e: any) => {
-    e.preventDefault();
-    const removed = portfolioValues.filter((value) => {
-      return value !== e.target[0].value;
-    });
-    setPortfolioValues(removed);
-  };
-
-  const onClick = async (e: SyntheticEvent)=>{
-    // console.log(e);
-    e.preventDefault();
-    const result = await searchCompanies(search);
-
-    if (typeof result === "string") {
-      setServerError(result);
-    } else if (Array.isArray(result.data)) {
-      setSearchResult(result.data);
-    }
-    // console.log("Search Result: ", searchResult);
-  }
-
-  console.log("App.tsx onPortfolioDelete function:", onPortfolioDelete);
-
-
+function App() {
   return (
-  <div>
-    <Navbar />
-    <Search onClick={onClick} search={search} handleChange={handleChange} />
-    <ListPortfolio portfolioValues={portfolioValues} onPortfolioDelete={onPortfolioDelete} />
-    <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
-    {serverError && <div>Unable to connect to API</div>}
-</div>
-);
-};
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+export default App;
