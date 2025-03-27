1aa909bbf80ebcb8fd5cc5bdc48109ecd74b3a05
vishal911m <vishal911m@gmail.com>
2025-03-27 12:51:52 +0530

    useEffect

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -10,11 +10,13 @@ const Hero = (props: Props) => {
        <div className="container flex flex-col-reverse mx-auto p-8 lg:flex-row">
          <div className="flex flex-col space-y-10 mb-44 m-10 lg:m-10 xl:m-20 lg:mt:16 lg:w-1/2 xl:mb-52">
            <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
-             Financial data with no news.
+             {/* Financial data with no news. */}
+             Supercharge Your Factory’s Performance
            </h1>
            <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
-             Search relevant financial documents without fear mongering and fake
-             news.
+             {/* Search relevant financial documents without fear mongering and fake
+             news. */}
+             Streamline operations with AI-powered efficiency solutions.
            </p>
            <div className="mx-auto lg:mx-0">
              <Link
--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../api';
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
+  let {ticker} = useParams();
+  const [company, setCompany] = useState<CompanyProfile>();
+
+  useEffect(()=>{
+    const getProfileInit = async () =>{
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, [])
+
   return (
-    <div>CompanyPage</div>
+    <div>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company Not Found!</div>
+      )}
+    </div>
   )
 }
 
