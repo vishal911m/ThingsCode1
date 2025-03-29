9806b29309fa6e778c983129e2e8e039a68a9412
vishal911m <vishal911m@gmail.com>
2025-03-29 20:39:50 +0530

    Dashboard

--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { CompanyProfile } from '../../company';
 import { getCompanyProfile } from '../../api';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import CompanyDashboard from '../../Components/CompanyDashboard/CompanyDashboard';
+import Tile from '../../Components/Tile/Tile';
 
 interface Props {}
 
@@ -20,7 +23,14 @@ const CompanyPage = (props: Props) => {
   return (
     <div>
       {company ? (
-        <div>{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+
+        <Sidebar />
+        <CompanyDashboard>
+          <Tile title='Company Name' subTitle={company.companyName}/>
+        </CompanyDashboard>
+
+      </div>
       ) : (
         <div>Company Not Found!</div>
       )}
