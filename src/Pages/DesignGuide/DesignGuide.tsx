39c7509291a1eabe603d7fff626c94663bd862f5
vishal911m <vishal911m@gmail.com>
2025-04-02 11:21:49 +0530

    Table

---
+++ src/Pages/DesignGuide/DesignGuide.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Table from '../../Components/Table/Table'
+
+type Props = {}
+
+const DesignGuide = (props: Props) => {
+  return (
+    <>
+    <h1>ThingsCode Design Page</h1>
+    <h2>This is ThingsCode's design page. This is where we will house various design aspects of the app</h2>
+    <Table />
+    </>
+  )
+}
+
+export default DesignGuide
