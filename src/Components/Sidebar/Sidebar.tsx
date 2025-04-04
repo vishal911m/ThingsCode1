b7d18e38701d941979f4cd45a2350d5730cc0076
vishal911m <vishal911m@gmail.com>
2025-04-04 15:15:23 +0530

    Cashflow Statement

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -44,6 +44,14 @@ const Sidebar = (props: Props) => {
                   <h6 className='ml-3'>Balance Sheet</h6>
                 </Link>
 
+                <Link 
+                  to="cashflow-statement" 
+                  className='flex md:min-w-full text-blueGray-500 text-medium uppercase font-bold block pt-1 pb-4 no-underline'
+                >
+                  <FaHome />
+                  <h6 className='ml-3'>Cashflow Statement</h6>
+                </Link>
+
               </div>
 
             </div>
--- src/Pages/CompanyPage/CompanyPage.tsx
+++ src/Pages/CompanyPage/CompanyPage.tsx
@@ -31,6 +31,9 @@ const CompanyPage = (props: Props) => {
           <Tile title="Price" subTitle={company.price.toString()} />
           <Tile title="DCF" subTitle={company.dcf.toString()} />
           <Tile title="Sector" subTitle={company.sector} />
+          <p className="bg-white shadow rounded text-medium font-medium text-gray-900 p-3 mt-1 m-4">
+              {company.description}
+          </p>
         </CompanyDashboard>
 
       </div>
